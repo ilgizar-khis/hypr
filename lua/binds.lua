f1b6fd0c4c6d8727995d1964bc4fda531ee0509e
razigli <razigli@artix.dev>
2026-09-12 19:04:34 +0500

    optimize function keys

--- lua/binds.lua
+++ lua/binds.lua
@@ -53,30 +53,26 @@ hl.bind(Super("mouse_up"), hl.dsp.focus({ workspace = "e-1" }))
 hl.bind(Super("mouse:272"), hl.dsp.window.drag(), { mouse = true, desc = "move window (mouse)" })
 hl.bind(Super("mouse:273"), hl.dsp.window.resize(), { mouse = true, desc = "resize window (mouse)" })
 
-hl.bind(
-	"XF86AudioRaiseVolume",
-	hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
-	{ locked = true, repeating = true }
-)
-hl.bind(
-	"XF86AudioLowerVolume",
-	hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
-	{ locked = true, repeating = true }
-)
-hl.bind(
-	"XF86AudioMute",
-	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
-	{ locked = true, repeating = true }
-)
-hl.bind(
-	"XF86AudioMicMute",
-	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
-	{ locked = true, repeating = true }
-)
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
+local fn_repeating = {
+	XF86AudioRaiseVolume = "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+",
+	XF86AudioLowerVolume = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-",
+	XF86AudioMute = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle",
+	XF86AudioMicMute = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle",
+	XF86MonBrightnessUp = "brightnessctl -e4 -n2 set 5%+",
+	XF86MonBrightnessDown = "brightnessctl -e4 -n2 set 5%-",
+}
 
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
+for key, cmd in pairs(fn_repeating) do
+	hl.bind(key, hl.dsp.exec_cmd(cmd), { locked = true, repeating = true })
+end
+
+local fn = {
+	XF86AudioNext = "playerctl next",
+	XF86AudioPause = "playerctl play-pause",
+	XF86AudioPlay = "playerctl play-pause",
+	XF86AudioPrev = "playerctl previous",
+}
+
+for key, cmd in pairs(fn) do
+	hl.bind(key, hl.dsp.exec_cmd(cmd), { locked = true })
+end
