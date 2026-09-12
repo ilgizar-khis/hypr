fc47d334ed6031e3886737e859e7ee148592649d
razigli <razigli@artix.dev>
2026-09-12 18:39:24 +0500

    remove useless lines

--- lua/binds.lua
+++ lua/binds.lua
@@ -14,14 +14,14 @@ local function Shift(...)
 	return table.concat({ "SHIFT", ... }, " + ")
 end
 
-local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 local terminal = "kitty"
 local fileManager = "thunar"
 local menu = "rofi -show drun"
--- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
+
 hl.bind(Super("Return"), hl.dsp.exec_cmd(terminal))
-local closeWindowBind = hl.bind(Super("C"), hl.dsp.window.close())
--- closeWindowBind:set_enabled(false)
+
+hl.bind(Super("C"), hl.dsp.window.close())
+
 hl.bind(
 	Super("M"),
 	hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'")
@@ -32,33 +32,26 @@ hl.bind(Super("D"), hl.dsp.exec_cmd(menu))
 hl.bind(Super("P"), hl.dsp.window.pseudo())
 hl.bind(Super("J"), hl.dsp.layout("togglesplit")) -- dwindle only
 
--- Move focus with mainMod + arrow keys
 hl.bind(Super("left"), hl.dsp.focus({ direction = "left" }))
 hl.bind(Super("right"), hl.dsp.focus({ direction = "right" }))
 hl.bind(Super("up"), hl.dsp.focus({ direction = "up" }))
 hl.bind(Super("down"), hl.dsp.focus({ direction = "down" }))
 
--- Switch workspaces with mainMod + [0-9]
--- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
 	local key = i % 10 -- 10 maps to key 0
 	hl.bind(Super(key), hl.dsp.focus({ workspace = i }))
 	hl.bind(Super(Shift(key)), hl.dsp.window.move({ workspace = i }))
 end
 
--- Example special workspace (scratchpad)
 hl.bind(Super("S"), hl.dsp.workspace.toggle_special("magic"))
 hl.bind(Super(Shift("S")), hl.dsp.window.move({ workspace = "special:magic" }))
 
--- Scroll through existing workspaces with mainMod + scroll
 hl.bind(Super("mouse_down"), hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(Super("mouse_up"), hl.dsp.focus({ workspace = "e-1" }))
 
--- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(Super("mouse:272"), hl.dsp.window.drag(), { mouse = true })
 hl.bind(Super("mouse:273"), hl.dsp.window.resize(), { mouse = true })
 
--- Laptop multimedia keys for volume and LCD brightness
 hl.bind(
 	"XF86AudioRaiseVolume",
 	hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
@@ -82,7 +75,6 @@ hl.bind(
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
--- Requires playerctl
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
