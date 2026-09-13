b00e078f3eee4ec54ffa420c3d63170b066782df
razigli <razigli@artix.dev>
2026-09-13 13:23:02 +0500

    add udiskie to autostarts

--- lua/events.lua
+++ lua/events.lua
@@ -1,10 +1,12 @@
 local terminal = "kitty"
 local swaybg = "swaybg"
 local waybar = "waybar"
+local disk = "udiskie"
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd(terminal)
 	hl.exec_cmd(waybar)
+	hl.exec_cmd(disk)
 	hl.exec_cmd(swaybg .. " -o HDMI-A-1 -i /home/razigli/backgrounds/GunsmithCats.jpg")
 	hl.exec_cmd(swaybg .. " -o eDP-1 -i /home/razigli/backgrounds/a24770.jpg")
 end)
