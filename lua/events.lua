35156f951c6007c237ac444db5445cd09ec5a8a0
razigli <razigli@artix.dev>
2026-09-13 12:47:05 +0500

    add waybar to autostarts

--- lua/events.lua
+++ lua/events.lua
@@ -1,8 +1,10 @@
 local terminal = "kitty"
 local swaybg = "swaybg"
+local waybar = "waybar"
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd(terminal)
+	hl.exec_cmd(waybar)
 	hl.exec_cmd(swaybg .. " -o HDMI-A-1 -i /home/razigli/backgrounds/GunsmithCats.jpg")
 	hl.exec_cmd(swaybg .. " -o eDP-1 -i /home/razigli/backgrounds/a24770.jpg")
 end)
