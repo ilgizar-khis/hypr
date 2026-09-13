14c210e3d57a2eacebcdb72b6807abe56bc73315
razigli <razigli@artix.dev>
2026-09-13 11:29:58 +0500

    update screenshot.sh

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/bash
 
 if [[ "$#" == 1 ]]; then
+	geometry=""
 	dir="$(xdg-user-dir PICTURES)/Screenshots"
 	file="$(date +'%d_%B_%Y_%H_%M_%S').png"
 	if [ ! -d "$dir" ]; then
 		mkdir -p "$dir"
 	fi
 
-	grim -g "$1" "$dir/$file"
+	if [[ "$1" == "region" ]]; then
+		geometry="$(slurp)"
+	elif [[ "$1" == "client" ]]; then
+		bs1="$(hyprctl repl 'hl.get_config("general.border_size")')"
+		bs2=$((bs * 2))
+		geometry="$(hyprctl activewindow -j | jq -r '"\(.at.[0] - '$bs1'),\(.at.[1] - '$bs1') \(.size.[0] + '$bs2')x\(.size.[1] + '$bs2')"')"
+	else
+		geometry="$(slurp -o)"
+	fi
+
+	grim -g "$geometry" "$dir/$file"
 
 	result="$(notify-send "$file" -i "$dir/$file" -t 2000 -a "Screenshot" -A "default=default")"
 
