10b3856d158e1701442b63d4c692cf9cf58dd9f3
razigli <razigli@artix.dev>
2026-09-13 11:44:05 +0500

    add wallpapers

--- lua/events.lua
+++ lua/events.lua
@@ -1,5 +1,8 @@
 local terminal = "kitty"
+local swaybg = "swaybg"
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd(terminal)
+	hl.exec_cmd(swaybg .. " -o HDMI-A-1 -i /home/razigli/backgrounds/GunsmithCats.jpg")
+	hl.exec_cmd(swaybg .. " -o eDP-1 -i /home/razigli/backgrounds/a24770.jpg")
 end)
